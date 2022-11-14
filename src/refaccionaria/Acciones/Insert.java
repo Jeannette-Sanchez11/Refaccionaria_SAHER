13c6e8eaef90bf08f899129e4b4979a7ad423d5e
Noe7733 <noe777guillenwigetta@gmail.com>
2022-11-14 02:17:19 -0600

    avance dos

--- src/refaccionaria/Acciones/Insert.java
+++ src/refaccionaria/Acciones/Insert.java
@@ -22,8 +22,80 @@ public class Insert {
     }
     
     public boolean insertEmpleado(String nombre_Emp,String ap_Pat,String ap_Mat, int status_Empleado, String tel_Empleado, String correo){//metodo para insertar un proveedor
+        //se prepara la insercion del dato de empleado
+        sql="insert into Empleado values('"+nombre_Emp+"','"+ap_Pat+"','"+ap_Mat+"',"+status_Empleado+",'"+tel_Empleado+"','"+correo+"');";
+        respuesta=true;// nos devuelven el valor booleano despues de la insercion
+        try{
+            st=conex.createStatement();
+            respuesta=st.execute(sql);
+            System.out.println(respuesta);
+        }catch(Exception e)
+        {
+             JOptionPane.showMessageDialog(null,e.getMessage() );
+        }finally{
+            try{
+                st.close();//cierra la conexion
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null,ex.getMessage() );
+                System.out.println(ex.getMessage());
+            }
+        }
+       
+        return !respuesta;//retorno de respuesta
+        
+    }
+    
+    public boolean insertCliente(String nombre,String ap_Pat,String ap_Mat, String telefono){//metodo para insertar un cliente
         //se prepara la insercion del dato de proveedor
-        sql="insert into materiales values('"+nombre_Emp+"','"+ap_Pat+"',"+ap_Mat+"',"+status_Empleado+",'"+tel_Empleado+"','"+correo+"');";
+        sql="insert into Cliente values('"+nombre+"','"+ap_Pat+"','"+ap_Mat+"','"+telefono+"');";
+        respuesta=true;// nos devuelven el valor booleano despues de la insercion
+        try{
+            st=conex.createStatement();
+            respuesta=st.execute(sql);
+            System.out.println(respuesta);
+        }catch(Exception e)
+        {
+             JOptionPane.showMessageDialog(null,e.getMessage() );
+        }finally{
+            try{
+                st.close();//cierra la conexion
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null,ex.getMessage() );
+                System.out.println(ex.getMessage());
+            }
+        }
+       
+        return !respuesta;//retorno de respuesta
+        
+    }
+    
+    public boolean insertTipoDeMoto(String modelo,String nombre_TipoM,String marca, int anio){//metodo para insertar un tipo de moto
+        //se prepara la insercion del dato de proveedor
+        sql="insert into Tipo_De_Moto values('"+modelo+"','"+nombre_TipoM+"','"+marca+"',"+anio+",');";
+        respuesta=true;// nos devuelven el valor booleano despues de la insercion
+        try{
+            st=conex.createStatement();
+            respuesta=st.execute(sql);
+            System.out.println(respuesta);
+        }catch(Exception e)
+        {
+             JOptionPane.showMessageDialog(null,e.getMessage() );
+        }finally{
+            try{
+                st.close();//cierra la conexion
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null,ex.getMessage() );
+                System.out.println(ex.getMessage());
+            }
+        }
+       
+        return !respuesta;//retorno de respuesta
+        
+    }
+    
+    public boolean insertArticulos(String codi_Barra,int id_Tipo,String nombre_Arti, String marca, float precio, int stock){//metodo para insertar un articulo
+        //se prepara la insercion del dato de Articulos
+        sql="insert into Articulos values('"+codi_Barra+"',"+id_Tipo+",'"+nombre_Arti+"','"+marca+"',"+precio+","+stock+");";
         respuesta=true;// nos devuelven el valor booleano despues de la insercion
         try{
             st=conex.createStatement();
@@ -45,4 +117,75 @@ public class Insert {
         
     }
  
+    public boolean insertServicios(String descripcion,int precio_Servi){//metodo para insertar un servicio
+        //se prepara la insercion del dato de servicios
+        sql="insert into Servicios values('"+descripcion+"',"+precio_Servi+");";
+        respuesta=true;// nos devuelven el valor booleano despues de la insercion
+        try{
+            st=conex.createStatement();
+            respuesta=st.execute(sql);
+            System.out.println(respuesta);
+        }catch(Exception e)
+        {
+             JOptionPane.showMessageDialog(null,e.getMessage() );
+        }finally{
+            try{
+                st.close();//cierra la conexion
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null,ex.getMessage() );
+                System.out.println(ex.getMessage());
+            }
+        }
+       
+        return !respuesta;//retorno de respuesta
+        
+    }
+    
+    public boolean insertProveedor(String rfc,String nombre,String telefono){//metodo para insertar un proveedor
+        //se prepara la insercion del dato de proveedor
+        sql="insert into Proveedor values('"+rfc+"','"+nombre+"','"+telefono+"');";
+        respuesta=true;// nos devuelven el valor booleano despues de la insercion
+        try{
+            st=conex.createStatement();
+            respuesta=st.execute(sql);
+            System.out.println(respuesta);
+        }catch(Exception e)
+        {
+             JOptionPane.showMessageDialog(null,e.getMessage() );
+        }finally{
+            try{
+                st.close();//cierra la conexion
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null,ex.getMessage() );
+                System.out.println(ex.getMessage());
+            }
+        }
+       
+        return !respuesta;//retorno de respuesta
+        
+    }
+
+    public boolean insertUsuarios(String nombre_usuario,String contrsenia){//metodo para insertar un usuario
+        //se prepara la insercion del dato de usuario
+        sql="insert into usuarios values('"+nombre_usuario+"','"+contrsenia+"');";
+        respuesta=true;// nos devuelven el valor booleano despues de la insercion
+        try{
+            st=conex.createStatement();
+            respuesta=st.execute(sql);
+            System.out.println(respuesta);
+        }catch(Exception e)
+        {
+             JOptionPane.showMessageDialog(null,e.getMessage() );
+        }finally{
+            try{
+                st.close();//cierra la conexion
+            }catch(Exception ex){
+                JOptionPane.showMessageDialog(null,ex.getMessage() );
+                System.out.println(ex.getMessage());
+            }
+        }
+       
+        return !respuesta;//retorno de respuesta
+        
+    }    
 }
--- src/refaccionaria/Acciones/Select.java
+++ src/refaccionaria/Acciones/Select.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package refaccionaria.Acciones;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Noe Guillen Gerardo
  */
 public class Select {
+ 
+    
+    Connection conex;
+    Conexion c = new Conexion();
+    Statement st;
+    ResultSet rs=null;
+    PreparedStatement ps=null;
+    String sql;//variable para recibir las sentencias de insercion
+    int respu;
+    
+    public Select(){
+        conex = c.ConectarBD();
+    }
+    
+    //seleccionar un empleado por medio del id (duda: status empleado se obtiene como un string o como un int )
+     public String[] selectEmpleado(int id_Emp){        
+        String sql="select * from usuarios where nombreUsu='"+id_Emp+"';";
+        String []x=new String[1];
+        try{
+           st=conex.createStatement();
+           rs=st.executeQuery(sql);
+           while(rs.next()){
+               x[0]=rs.getString("nombre_Emp");
+               x[1]=rs.getString("ap_Pat");
+               x[2]=rs.getString("ap_Mat");
+               x[3]=rs.getString("status_Empleado");
+               x[4]=rs.getString("tel_Empleado");
+               x[5]=rs.getString("correo");
+           }  
+        }catch(Exception e){
+           JOptionPane.showMessageDialog(null,e.getMessage() );
+           System.out.println(e.getMessage());
+        }finally{
+            try {
+                st.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,ex.getMessage() );
+            }
+        }
+        return x;
+    }
     
 }
